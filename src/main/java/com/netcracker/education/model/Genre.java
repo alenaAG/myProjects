d6fdf56ac53e5a5c981e9892053d6dbec36f14c1
goonerpi <pavelmokshanov@gmail.com>
2016-11-08 11:03:57 +0400

    Add files via upload

---
+++ src/main/java/com/netcracker/education/model/Genre.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Genre {
+	private int id;
+	private String genreName;
+	private List trackList;
+	
+	public int getId(){
+		return id;
+		}
+	public void setId(int id){
+		this.id=id;
+		}
+    public String getGenreName(){
+		return songName;
+		}
+	public void setGenreName(String genreName){
+		this.genreName=genreName;
+		}
+   	public List getTrackList(){
+		return trackList;
+		}
+	public void setTrackList(List trackList){
+		this.trackList=trackList;
+		}
+}
